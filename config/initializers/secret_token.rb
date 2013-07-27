b99a57b8cd90a2cd7b932aea3797bb46ecb5d082
Vingilot <fnetma@hotmail.com>
2013-07-27 22:15:54 +0000

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Happs::Application.config.secret_key_base = '3a65bb5e442685d7441f6f232bff53b76b0f84beb7cc537d458787b596d22f42b4d1417a3e48867129c70742b50996323b8e87f377f051f91a3042c3225760f0'
