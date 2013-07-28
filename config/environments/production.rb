2c736e8abd725f182d5f8445abdc4a1fe4223e17
Vingilot <fnetma@hotmail.com>
2013-07-28 05:37:12 +0000

    Fixed precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,8 +58,8 @@ Happs::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets.
-  application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
+  # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+  config.assets.precompile += %w( search.js )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
