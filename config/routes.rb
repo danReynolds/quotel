91762f4009af0a0b3ce3bb06229f0c50bd0786b2
Vingilot <fnetma@hotmail.com>
2013-08-16 03:08:51 +0000

    Atom feed location changed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Happs::Application.routes.draw do
-  get "welcome/index"
-  get "welcome/profile"
+  resources :welcome, only: [:index]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   
